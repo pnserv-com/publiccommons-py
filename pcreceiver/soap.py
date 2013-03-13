2ee01c3444adf5d54bf943a16edc2f2b74493c12
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-03-13 19:12:27 +0900

    support xml namespaces

--- pcreceiver/soap.py
+++ pcreceiver/soap.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 import lxml.etree
 from spyne.decorator import srpc
 from spyne.service import ServiceBase
@@ -28,32 +30,47 @@ class MQService(ServiceBase):
             f.write(lxml.etree.tostring(message))
         result = parse(message)
         print(result)
+        print(result.find('edxlde:distributionID'))
         return PublishResponse(response=ProcessResponse(code=0))
 
 
-def parse(elem):
-    d = Message()
-    for el in elem:
-        tag = el.tag.split('}')[-1]
-        d[tag] = parse(el) if len(el) > 0 else el.text
+class XMLDict(dict):
+    namespace_re = re.compile(r'{([^}]+)}(.+)')
 
-    return d
+    def __init__(self, ns=None):
+        super(XMLDict, self).__init__()
+        self.ns = ns or {}
+        self.rns = {v: k for k, v in self.ns.items()}
 
+    def __setitem__(self, key, value):
+        m = XMLDict.namespace_re.match(key)
+        if m:
+            alias = self.rns.get(m.group(1))
+            if alias:
+                key = '{}:{}'.format(alias, m.group(2))
+            else:
+                key = m.group(2)
 
-class Message(dict):
-    def __getattr__(self, key):
-        if key in self:
-            return self[key]
-        raise AttributeError(key)
+        super(XMLDict, self).__setitem__(key, value)
 
     def find(self, key):
         if key in self:
             return self[key]
 
-        for v in (x for x in self.values() if isinstance(x, Message)):
-            return v.find(key)
+        for v in (x for x in self.values() if isinstance(x, XMLDict)):
+            r = v.find(key)
+            if r:
+                return r
 
         return None
+
+
+def parse(elem):
+    d = XMLDict(elem.nsmap)
+    for el in elem:
+        d[el.tag] = parse(el) if len(el) > 0 else el.text
+
+    return d
 
 
 application = wsgi_soap_application([MQService], TARGET_NAMESPACE)
