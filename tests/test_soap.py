ffb0599cdaf9b42e144d35af5d742998f2dae7b4
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-03-14 14:11:31 +0900

    add tests

---
+++ tests/test_soap.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from pcreceiver import soap
+
+
+class TestXMLDict(object):
+    nsmap = {
+        'ns1': 'http://example.com/ns1',
+        'ns2': 'http://example.com/ns2'
+    }
+
+    def test_init(self):
+        d = soap.XMLDict(TestXMLDict.nsmap)
+        assert d.ns == TestXMLDict.nsmap
+        assert d.rns == {
+        'http://example.com/ns1': 'ns1',
+        'http://example.com/ns2': 'ns2'
+    }
+
+    def test_setitem(self):
+        d = soap.XMLDict(TestXMLDict.nsmap)
+        d['{http://example.com/ns1}foo'] = 1
+        d['{http://example.com/ns2}foo'] = 2
+        d['{http://example.com/ns3}foo'] = 3
+        assert d['ns1:foo'] == 1
+        assert d['ns2:foo'] == 2
+        assert d['foo'] == 3
+
+    def test_find(self):
+        d = soap.XMLDict()
+        d['foo'] = 1
+        d['nest'] = soap.XMLDict()
+        d['nest']['bar'] = 2
+        d['nest2'] = soap.XMLDict()
+        d['nest2']['baz'] = 3
+        assert d.find('foo') == 1
+        assert d.find('bar') == 2
+        assert d.find('baz') == 3
+        assert d.find('nothing') is None
