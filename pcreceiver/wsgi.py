1ff4c10b3c5cc9ee994496a79742f0ebb8f51a5b
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-04-08 17:41:31 +0900

    add wsgi middleware to record request body

--- pcreceiver/wsgi.py
+++ pcreceiver/wsgi.py
@@ -6,6 +6,11 @@ from ConfigParser import SafeConfigParser
 
 from pcreceiver import nckvs
 
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
 
 def search_config():
     path = os.getenv('PCRECEIVER_CONFIG')
@@ -20,6 +25,19 @@ def search_config():
     return None
 
 
+class RequestLogger(object):
+    def __init__(self, application, logger_name='request.body'):
+        self.application = application
+        self.log = logging.getLogger(logger_name)
+
+    def __call__(self, environ, start_response):
+        length = int(environ.get('CONTENT_LENGTH', '0'))
+        body = StringIO(environ['wsgi.input'].read(length))
+        self.log.info(body.getvalue())
+        environ['wsgi.input'] = body
+        return self.application(environ, start_response)
+
+
 config = search_config()
 logging.config.fileConfig(config)
 
@@ -29,6 +47,7 @@ config = dict(parser.items('nckvs'))
 nckvs.setup(**config)
 
 from pcreceiver.soap import application
+application = RequestLogger(application)
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
